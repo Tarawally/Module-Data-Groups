cdf3372565d55851e8d7d5dc3a26f24e154c99a5
Tarawally <41825140+Tarawally@users.noreply.github.com.>
2025-07-26 12:00:31 +0000

    fix(author.js): object property valuDe in loop

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -3,6 +3,8 @@
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
+// The reason why it isn't working is that the for loop is not correctly iterating over the values of the object.
+
 const author = {
   firstName: "Zadie",
   lastName: "Smith",
@@ -11,6 +13,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
